4485ba8dd79eeac7e92387a6bb117e31905d7864
lasyasrikandukuri-099 <lasyasrikandukuri@gmail.com>
2026-08-12 16:47:53 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RFAnalyzer"
+include(":app")
+include(":nativedsp")
+include(":libhackrf")
+include(":libairspy")
+include(":libusb")
+include(":libhydrasdr")
+include(":libairspyhf")
